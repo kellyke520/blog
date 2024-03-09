3801ed981ae0c26a164d2ac68b90d9b10c97fa8b
kelin <1464404227@qq.com>
2024-03-09 23:24:56 +0800

    配置更改

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f https://github.com/kelin96/kelin1996.github.io.git master:gh-pages
+git push -f https://github.com/kelin96/kelin96.github.io.git master:gh-pages
 
 cd -
